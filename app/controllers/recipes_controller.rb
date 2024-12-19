ff659a8bae1874dab580002b10f9c44dcb8141d3
Jaime Barros <jabgnecco@gmail.com>
2024-12-19 13:39:07 +0000

    added routes views and buttons

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,53 @@
+class RecipesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_recipe, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = current_user.recipes.new(recipe_params) # Associate recipe with the logged-in user
+    if @recipe.save
+      redirect_to @recipe, notice: 'Recipe was successfully created.'
+    else
+      flash.now[:alert] = 'There was an error creating the recipe.'
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+    @recipe.update(recipe_params)
+    redirect_to recipe_path(@recipe)
+  end
+
+  def destroy
+
+    @recipe.destroy
+    redirect_to recipes_path, status: :see_other
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :description, :ingredients, :instructions, :category, :difficulty)
+  end
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,11 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  has_many :reviews, dependent: :destroy
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :ingredients, presence: true
+  validates :instructions, presence: true
+  validates :category, presence: true
+  validates :difficulty, presence: true
 end
